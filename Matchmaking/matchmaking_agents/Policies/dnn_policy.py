3f1a612e4d720e29e3886711088bde67d1972822
SPhax <sbkerbrat@gmail.com>
2018-07-15 11:35:41 +0200

    Use baselines functions for creating the models

--- Matchmaking/matchmaking_agents/Policies/dnn_policy.py
+++ Matchmaking/matchmaking_agents/Policies/dnn_policy.py
@@ -1,8 +1,59 @@
+from gym.spaces import Discrete, Box
 import math
 from matchmaking_agents.Policies.policy import Policy
 import tensorflow as tf
 import numpy as np
 
+def observation_input(ob_space, batch_size=None, name='Ob'):
+    '''
+    Build observation input with encoding depending on the 
+    observation space type
+    Params:
+    
+    ob_space: observation space (should be one of gym.spaces)
+    batch_size: batch size for input (default is None, so that resulting input placeholder can take tensors with any batch size)
+    name: tensorflow variable name for input placeholder
+
+    returns: tuple (input_placeholder, processed_input_tensor)
+    '''
+    if isinstance(ob_space, Discrete):
+        input_x  = tf.placeholder(shape=(batch_size,), dtype=tf.int32, name=name)
+        processed_x = tf.to_float(tf.one_hot(input_x, ob_space.n))
+        return input_x, processed_x
+
+    elif isinstance(ob_space, Box):
+        input_shape = (batch_size,) + ob_space.shape
+        input_x = tf.placeholder(shape=input_shape, dtype=ob_space.dtype, name=name)
+        processed_x = tf.to_float(input_x)
+        return input_x, processed_x
+
+    else:
+        raise NotImplementedError
+
+
+def ortho_init(scale=1.0):
+    def _ortho_init(shape, dtype, partition_info=None):
+        #lasagne ortho init for tf
+        shape = tuple(shape)
+        if len(shape) == 2:
+            flat_shape = shape
+        elif len(shape) == 4: # assumes NHWC
+            flat_shape = (np.prod(shape[:-1]), shape[-1])
+        else:
+            raise NotImplementedError
+        a = np.random.normal(0.0, 1.0, flat_shape)
+        u, _, v = np.linalg.svd(a, full_matrices=False)
+        q = u if u.shape == flat_shape else v # pick the one with the correct shape
+        q = q.reshape(shape)
+        return (scale * q[:shape[0], :shape[1]]).astype(np.float32)
+    return _ortho_init
+
+def fc(x, scope, nh, *, init_scale=1.0, init_bias=0.0):
+    with tf.variable_scope(scope):
+        nin = x.get_shape()[1].value
+        w = tf.get_variable("w", [nin, nh], initializer=ortho_init(init_scale))
+        b = tf.get_variable("b", [nh], initializer=tf.constant_initializer(init_bias))
+        return tf.matmul(x, w)+b
 
 class CategoricalPd(object):
     def __init__(self, logits):
@@ -29,24 +80,16 @@ class DNNPolicy(Policy):
 
     def create_model(self, env, num_layers):
         with tf.variable_scope("policy", reuse=False):
-            self.X = tf.placeholder(shape=[None, self.input_size], dtype=env.observation_space.dtype, name="X")
+            self.X, processed_x = observation_input(env.observation_space, None)
+            activ = tf.tanh
+            processed_x = tf.layers.flatten(processed_x)
 
-            previous_layer = self.X
+            previous_layer = processed_x
             for idx in range(num_layers):
-                hidden_layer = tf.contrib.layers.fully_connected(
-                    inputs=previous_layer,
-                    num_outputs=64,
-                    activation_fn=tf.nn.tanh,
-                    weights_initializer=tf.constant_initializer(np.sqrt(2))
-                )
+                hidden_layer = activ(fc(previous_layer, 'pi_fc'+str(idx), nh=64, init_scale=np.sqrt(2)))
                 previous_layer = hidden_layer
 
-            self.output_layer = tf.contrib.layers.fully_connected(
-                inputs=previous_layer,
-                num_outputs=self.output_size,
-                activation_fn=None,
-                weights_initializer=tf.zeros_initializer
-            )
+            self.output_layer = fc(previous_layer, 'pi', self.output_size, init_scale=0.01, init_bias=0.0)
 
             self.probability_distribution = CategoricalPd(self.output_layer)
 
--- Matchmaking/matchmaking_agents/Values/dnn_value.py
+++ Matchmaking/matchmaking_agents/Values/dnn_value.py
@@ -1,8 +1,61 @@
+from gym.spaces import Discrete, Box
 import math
 import matchmaking_agents.agents
 from matchmaking_agents.Values.value import MatchmakingValue
 import tensorflow as tf
 import numpy as np
+
+
+def observation_input(ob_space, batch_size=None, name='Ob'):
+    '''
+    Build observation input with encoding depending on the 
+    observation space type
+    Params:
+    
+    ob_space: observation space (should be one of gym.spaces)
+    batch_size: batch size for input (default is None, so that resulting input placeholder can take tensors with any batch size)
+    name: tensorflow variable name for input placeholder
+
+    returns: tuple (input_placeholder, processed_input_tensor)
+    '''
+    if isinstance(ob_space, Discrete):
+        input_x  = tf.placeholder(shape=(batch_size,), dtype=tf.int32, name=name)
+        processed_x = tf.to_float(tf.one_hot(input_x, ob_space.n))
+        return input_x, processed_x
+
+    elif isinstance(ob_space, Box):
+        input_shape = (batch_size,) + ob_space.shape
+        input_x = tf.placeholder(shape=input_shape, dtype=ob_space.dtype, name=name)
+        processed_x = tf.to_float(input_x)
+        return input_x, processed_x
+
+    else:
+        raise NotImplementedError
+
+
+def ortho_init(scale=1.0):
+    def _ortho_init(shape, dtype, partition_info=None):
+        #lasagne ortho init for tf
+        shape = tuple(shape)
+        if len(shape) == 2:
+            flat_shape = shape
+        elif len(shape) == 4: # assumes NHWC
+            flat_shape = (np.prod(shape[:-1]), shape[-1])
+        else:
+            raise NotImplementedError
+        a = np.random.normal(0.0, 1.0, flat_shape)
+        u, _, v = np.linalg.svd(a, full_matrices=False)
+        q = u if u.shape == flat_shape else v # pick the one with the correct shape
+        q = q.reshape(shape)
+        return (scale * q[:shape[0], :shape[1]]).astype(np.float32)
+    return _ortho_init
+
+def fc(x, scope, nh, *, init_scale=1.0, init_bias=0.0):
+    with tf.variable_scope(scope):
+        nin = x.get_shape()[1].value
+        w = tf.get_variable("w", [nin, nh], initializer=ortho_init(init_scale))
+        b = tf.get_variable("b", [nh], initializer=tf.constant_initializer(init_bias))
+        return tf.matmul(x, w)+b
 
 
 class DNNValue(MatchmakingValue):
@@ -13,24 +66,17 @@ class DNNValue(MatchmakingValue):
 
     def create_model(self, env, num_layers):
         with tf.variable_scope("value", reuse=False):
-            self.X = tf.placeholder(shape=[None, self.input_size], dtype=env.observation_space.dtype, name="X")
+            self.X, processed_x = observation_input(env.observation_space, None)
+            activ = tf.tanh
+            processed_x = tf.layers.flatten(processed_x)
 
-            previous_layer = self.X
+            previous_layer = processed_x
             for idx in range(num_layers):
-                hidden_layer = tf.contrib.layers.fully_connected(
-                    inputs=previous_layer,
-                    num_outputs=16,
-                    activation_fn=tf.nn.tanh,
-                    weights_initializer=tf.constant_initializer(np.sqrt(2))
-                )
+                hidden_layer = activ(fc(previous_layer, 'value_fc'+str(idx), nh=64, init_scale=np.sqrt(2)))
                 previous_layer = hidden_layer
 
-            self.value = tf.squeeze(tf.contrib.layers.fully_connected(
-                inputs=self.X,
-                num_outputs=1,
-                activation_fn=None,
-                weights_initializer=tf.zeros_initializer
-            ))
+            self.value = fc(previous_layer, 'vf', 1)[:,0]
+
 
         self.OLD_VALUES = tf.placeholder(tf.float32, [None])
         self.RETURNS = tf.placeholder(tf.float32, [None])
