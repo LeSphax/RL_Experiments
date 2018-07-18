Fix normalisation, Add grad norm, add session config

Matchmaking/matchmaking_agents/Policies/dnn_policy.py
@@ -110,10 +110,16 @@ class DNNPolicy(Policy):
         pg_losses = -self.ADVANTAGES * ratio
         pg_losses2 = -self.ADVANTAGES * tf.clip_by_value(ratio, 1.0 - self.CLIPPING, 1.0 + self.CLIPPING)
         self.loss = tf.reduce_mean(tf.maximum(pg_losses, pg_losses2)) - self.entropy * 0.01
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE)
-        self.train = optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
+       
+        with tf.variable_scope('policy'):
+            params = tf.trainable_variables()
+        grads = tf.gradients(self.loss, params)
+        grads, _grad_norm = tf.clip_by_global_norm(grads, 0.5)
+        grads = list(zip(grads, params))
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE, epsilon=1e-5)
+        self._train = optimizer.apply_gradients(grads)
 
-        self.sess = tf.Session()
+        self.sess = tf.get_default_session()
         init = tf.global_variables_initializer()
         self.sess.run(init)
 
@@ -125,7 +131,7 @@ class DNNPolicy(Policy):
     def train_model(self, obs, actions, neglogp_actions, advantages, clipping, learning_rate):
         advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
         entropy, loss, _ = self.sess.run(
-            [self.entropy, self.loss, self.train],
+            [self.entropy, self.loss, self._train],
             {
                 self.X: obs,
                 self.ACTIONS: actions,

Matchmaking/matchmaking_agents/Values/dnn_value.py
@@ -87,10 +87,16 @@ class DNNValue(MatchmakingValue):
         losses1 = tf.square(self.value - self.RETURNS)
         losses2 = tf.square(value_clipped - self.RETURNS)
         self.loss = .5 * tf.reduce_mean(tf.maximum(losses1, losses2))
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE)
-        self.train = optimizer.minimize(self.loss, global_step=tf.train.get_global_step())
 
-        self.sess = tf.Session()
+        with tf.variable_scope('value'):
+            params = tf.trainable_variables()
+        grads = tf.gradients(self.loss, params)
+        grads, _grad_norm = tf.clip_by_global_norm(grads, 0.5)
+        grads = list(zip(grads, params))
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE, epsilon=1e-5)
+        self._train = optimizer.apply_gradients(grads)
+
+        self.sess = tf.get_default_session()
         init = tf.global_variables_initializer()
         self.sess.run(init)
 
@@ -100,7 +106,7 @@ class DNNValue(MatchmakingValue):
 
     def train_model(self, obs, values, returns, clipping, learning_rate):
         values, loss, _ = self.sess.run(
-            [self.value, self.loss, self.train],
+            [self.value, self.loss, self._train],
             {
                 self.X: obs,
                 self.OLD_VALUES: values,

Matchmaking/my_ppo.py
@@ -16,6 +16,7 @@ from utils.gae import gae
 import utils.keyPoller as kp
 import random
 import time
+import multiprocessing
 from docopt import docopt
 
 
@@ -49,6 +50,7 @@ date = datetime.now().strftime("%m%d-%H%M")
 class EnvRunner(object):
     def __init__(self, env, value_estimator, policy_estimator, discount_factor=0.99, gae_weighting=0.95):
         self.env = AutoResetEnv(env)
+        self.env = NormalizeEnv(self.env)
         self.obs = self.env.reset()
         self.value_estimator = value_estimator
         self.policy_estimator = policy_estimator
@@ -115,6 +117,16 @@ class EnvRunner(object):
 
 def simulate():
 
+    ncpu = multiprocessing.cpu_count()
+    if sys.platform == 'darwin':
+        ncpu //= 2
+    config = tf.ConfigProto(allow_soft_placement=True,
+                            intra_op_parallelism_threads=ncpu,
+                            inter_op_parallelism_threads=ncpu)
+    config.gpu_options.allow_growth = True  # pylint: disable=E1101
+    sess = tf.Session(config=config)
+    sess.__enter__()
+
     policy_estimator = dnn_policy.DNNPolicy(env, 2)
     value_estimator = dnn_value.DNNValue(env, 2)
 
@@ -131,8 +143,6 @@ def simulate():
     tf.summary.scalar('total_reward', TOTAL_REWARD)
 
     merged = tf.summary.merge_all()
-    global_step = tf.Variable(0, name="global_step", trainable=False)
-    sess = tf.Session()
     train_writer = tf.summary.FileWriter('./train/{name}_{date}'.format(name=name, date=date), sess.graph)
 
     training_batch = []

Matchmaking/wrappers/normalize_env.py
@@ -21,7 +21,7 @@ class NormalizeEnv(Wrapper):
         if self.ret_rms:
             self.ret_rms.update(self.ret)
             rews = np.clip(rews / np.sqrt(self.ret_rms.var + self.epsilon), -self.cliprew, self.cliprew)
-        return obs, rews, done, infos
+        return obs, rews[0], done, infos
 
     def _obfilt(self, obs):
         if self.ob_rms:
@@ -43,23 +43,16 @@ class RunningMeanStd(object):
         self.var = np.ones(shape, 'float64')
         self.count = epsilon
 
-    def update(self, x):
-        batch_mean = np.mean(x, axis=0)
-        batch_var = np.var(x, axis=0)
-        batch_count = x.shape[0]
-        self.update_from_moments(batch_mean, batch_var, batch_count)
+    def update(self, values):
+        delta = values - self.mean
+        tot_count = self.count + 1
 
-    def update_from_moments(self, batch_mean, batch_var, batch_count):
-        delta = batch_mean - self.mean
-        tot_count = self.count + batch_count
-
-        new_mean = self.mean + delta * batch_count / tot_count        
+        new_mean = self.mean + delta / tot_count        
         m_a = self.var * (self.count)
-        m_b = batch_var * (batch_count)
-        M2 = m_a + m_b + np.square(delta) * self.count * batch_count / (self.count + batch_count)
-        new_var = M2 / (self.count + batch_count)
+        M2 = m_a + np.square(delta) * self.count / (tot_count)
+        new_var = M2 / (tot_count)
 
-        new_count = batch_count + self.count
+        new_count = tot_count
 
         self.mean = new_mean
         self.var = new_var

Matchmaking/wrappers/tensorboard_env.py
@@ -18,7 +18,7 @@ class TensorboardEnv(Wrapper):
         tf.summary.scalar('total_reward', self.TOTAL_REWARD)
 
         self.merged = tf.summary.merge_all()
-        self.sess = tf.Session()
+        self.sess = tf.get_default_session()
         self.train_writer = tf.summary.FileWriter('../../../Experiments/Matchmaking/train/{name}'.format(name=name), self.sess.graph)
 
 